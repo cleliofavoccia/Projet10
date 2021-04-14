2a30c3481cf860a1a600cf1f841ba69f8d34d5fe
Clélio Favoccia <favoccia.c@live.fr>
2021-04-14 16:22:36 +0200

    Correction base.py

--- pur_beurre/settings/base.py
+++ pur_beurre/settings/base.py
@@ -141,7 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = os.path.join(BASE_DIR, "../../staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
